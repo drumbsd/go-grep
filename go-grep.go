bfbe15e975490006aac89b3939f119b68b595bb1
Matteo Niccoli <matteo@chorally.com>
2019-11-07 13:52:22 +0000

    Added some colour + avoid segfault when launched without parameters

--- go-grep.go
+++ go-grep.go
@@ -8,11 +8,16 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"github.com/gookit/color"
+	"strconv"
 )
 
 var root, query string
 var found = 1
 var wg sync.WaitGroup
+var red = color.FgRed.Render
+var green = color.FgGreen.Render
+var yellow  = color.FgYellow.Render
 
 func readFile(wg *sync.WaitGroup, path string) {
 	defer wg.Done()
@@ -27,7 +32,8 @@ func readFile(wg *sync.WaitGroup, path string) {
 	for i := 1; scanner.Scan(); i++ {
 		if strings.Contains(scanner.Text(), query) {
 			found = 0
-			fmt.Printf("%s/%s:%d: %s\n", root, path, i, scanner.Text())
+			fmt.Printf("%s/%s:%s: %s\n", red(root), green(path), yellow(strconv.Itoa(i)), scanner.Text())
+			
 		}
 	}
 }
@@ -36,14 +42,18 @@ func main() {
 	flag.Parse()
 	query = flag.Arg(0)
 	root = flag.Arg(1)
-
-	filepath.Walk(root, func(path string, file os.FileInfo, err error) error {
-		if !file.IsDir() {
+	
+	if query != "" && root != "" {
+	   filepath.Walk(root, func(path string, file os.FileInfo, err error) error {
+		  if !file.IsDir() {
 			wg.Add(1)
 			go readFile(&wg, path)
-		}
-		return nil
-	})
+		  } 
+		 return nil
+	   })
+	} else {
+		fmt.Println("Specify both pattern and filename")
+	}
 	wg.Wait()
 	defer os.Exit(found)
 }
